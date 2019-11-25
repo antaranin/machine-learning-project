db774001991b60b2a8d150aa863243dd68857c44
antaranin <rafal@getlevelup.com>
2019-11-25 15:22:04 +0100

    Added word2vector model

--- cnn.py
+++ cnn.py
@@ -1,2 +1,7 @@
 import numpy as np
+import gensim
 import tensorflow as tf
+
+#model = gensim.models.Word2Vec.load("./vectors/news_vectors.model")
+#data = np.load("./vectors/news_vectors.model.vectors.npy")
+#print(data)
