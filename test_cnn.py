4942f8afdef614fcc3ebb24178fd737d2aa4ca8c
antaranin <rafal@getlevelup.com>
2019-12-06 15:19:36 +0100

    Cleaned up the cnn class Removed TensorBoard code

--- test_cnn.py
+++ test_cnn.py
@@ -1,89 +1,104 @@
-import tensorflow as tf
+from typing import Tuple, Union, List
+
 import numpy as np
+import tensorflow as tf
 
 
-class TextCNN(object):
-    """
-    A CNN for text classification.
-    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
-    """
+class CNN(object):
 
     def __init__(
             self, sequence_length, num_classes, vocab_size,
-            embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0, embedding=None):
-        # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+            embedding_size, filter_sizes, num_filters, embedding=None):
+        self._setup_placeholders(sequence_length, num_classes)
+        self._setup_embedding(vocab_size, embedding_size, embedding)
+        self._setup_convolutional_pooling_layer(filter_sizes, embedding_size, num_filters,
+                                                sequence_length)
+        self._setup_dropout_layer()
 
-        # Keeping track of l2 regularization loss (optional)
-        l2_loss = tf.constant(0.0)
+        total_filter_count = num_filters * len(filter_sizes)
+        self._setup_output_layer(total_filter_count, num_classes)
+        self._setup_loss_calculation()
+        self._setup_accuracy_calculation()
 
-        # Embedding layer
+    def _setup_placeholders(self, sequence_length: int, number_of_classes: int):
+        self.input_data = tf.placeholder(tf.int32, [None, sequence_length])
+        self.input_labels = tf.placeholder(tf.float32, [None, number_of_classes])
+        self.dropout_keep_prob = tf.placeholder(tf.float32)
+
+    def _setup_embedding(self, vocabulary_size: int, embedding_size: int,
+                         embedding: np.ndarray = None):
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            self.W = tf.Variable(
-                tf.random_uniform([vocab_size, embedding_size], -1.0,
-                                  1.0) if embedding is None else embedding,
-                name="W", dtype='float32')
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            embedding_to_use = embedding if embedding is not None \
+                else tf.random_uniform((vocabulary_size, embedding_size), -1.0, 1.0)
+
+            self.W = tf.Variable(embedding_to_use, dtype='float32')
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_data)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
-        print(f"Embedded chars {self.embedded_chars}")
-        print(f"Chars expanded {self.embedded_chars_expanded}")
-
-        # Create a convolution + maxpool layer for each filter size
+    def _setup_convolutional_pooling_layer(self, filter_sizes: Union[List[int], Tuple[int]],
+                                           embedding_size: int,
+                                           number_of_filters: int, sequence_length: int):
         pooled_outputs = []
-        for i, filter_size in enumerate(filter_sizes):
-            with tf.name_scope("conv-maxpool-%s" % filter_size):
-                # Convolution Layer
-                filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                conv = tf.nn.conv2d(
-                    self.embedded_chars_expanded,
-                    W,
-                    strides=[1, 1, 1, 1],
-                    padding="VALID",
-                    name="conv")
-                # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-                # Maxpooling over the outputs
-                pooled = tf.nn.max_pool(
-                    h,
-                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
-                    strides=[1, 1, 1, 1],
-                    padding='VALID',
-                    name="pool")
-                pooled_outputs.append(pooled)
+        for filter_size in filter_sizes:
+            pooled = self._setup_convolutional_pooling_layer_for_filter(filter_size,
+                                                                        number_of_filters,
+                                                                        embedding_size,
+                                                                        sequence_length)
+            pooled_outputs.append(pooled)
 
         # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
+        total_filter_count = number_of_filters * len(filter_sizes)
         self.h_pool = tf.concat(pooled_outputs, 3)
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, total_filter_count])
 
-        # Add dropout
+    def _setup_convolutional_pooling_layer_for_filter(self, filter_size: int,
+                                                      number_of_filters: int, embedding_size: int,
+                                                      sequence_length: int):
+        with tf.name_scope(f"convolutional-maxpool-for-filter-{filter_size}"):
+            # Convolution Layer
+            filter_shape = (filter_size, embedding_size, 1, number_of_filters)
+            weights = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
+            bias = tf.Variable(tf.constant(0.1, shape=(number_of_filters,)))
+            conv = tf.nn.conv2d(
+                self.embedded_chars_expanded,
+                weights,
+                strides=(1, 1, 1, 1),
+                padding="VALID")
+            # Apply nonlinearity
+
+            feature_map = tf.nn.relu(tf.nn.bias_add(conv, bias), name="relu")
+
+            # Maxpooling over the outputs
+            pooled = tf.nn.max_pool(
+                feature_map,
+                ksize=(1, sequence_length - filter_size + 1, 1, 1),
+                strides=(1, 1, 1, 1),
+                padding='VALID')
+            return pooled
+
+    def _setup_dropout_layer(self):
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
-        # Final (unnormalized) scores and predictions
+    def _setup_output_layer(self, total_filter_count: int, number_of_classes: int):
         with tf.name_scope("output"):
-            W = tf.get_variable(
+            weights = tf.get_variable(
                 "W",
-                shape=[num_filters_total, num_classes],
+                shape=[total_filter_count, number_of_classes],
                 initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            bias = tf.Variable(tf.constant(0.1, shape=[number_of_classes]))
+            self.scores = tf.nn.xw_plus_b(self.h_drop, weights, bias)
+            self.predictions = tf.argmax(self.scores, 1)
 
         # Calculate mean cross-entropy loss
+
+    def _setup_loss_calculation(self):
         with tf.name_scope("loss"):
             losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores,
-                                                             labels=self.input_y)
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+                                                             labels=self.input_labels)
+            self.loss = tf.reduce_mean(losses)
 
-        # Accuracy
+    def _setup_accuracy_calculation(self):
         with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_labels, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
--- train.py
+++ train.py
@@ -1,47 +1,18 @@
+import datetime
 from typing import List, Dict
 
-import tensorflow as tf
 import numpy as np
-import os
-import time
-import datetime
-import tensorflow.data as tfd
+import tensorflow as tf
 
-from tensorflow.contrib.learn.python.learn.preprocessing import CategoricalVocabulary
-
-from test_cnn import TextCNN
 import data_importer as data
-from tensorflow.contrib import learn
-
-# Parameters
-# ==================================================
-
+from config import *
+from test_cnn import CNN
 # Data loading params
 from word2vec_handler import load_mr_word_vectors
 
-tf.flags.DEFINE_float("dev_sample_percentage", .1,
-                      "Percentage of the training data to use for validation")
 
-# Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 300,
-                        "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
-tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
-
-# Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100,
-                        "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
-tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-
-FLAGS = tf.flags.FLAGS
+# Parameters
+# ==================================================
 
 
 def preprocessing():
@@ -67,7 +38,7 @@ def preprocessing():
     print(f"X shuffled => {x_shuffled}")
 
     # Split train/test set
-    dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+    dev_sample_index = -1 * int(TEST_SPLIT_RATIO * float(len(y)))
     x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
     y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
 
@@ -125,19 +96,19 @@ def train(x_train, y_train, vocabulary, x_dev, y_dev, embedding_vectors=None):
     # ==================================================
 
     with tf.Graph().as_default():
-        session_conf = tf.ConfigProto(
-            allow_soft_placement=FLAGS.allow_soft_placement,
-            log_device_placement=FLAGS.log_device_placement)
-        sess = tf.Session(config=session_conf)
+        # session_conf = tf.ConfigProto(
+        #     allow_soft_placement=FLAGS.allow_soft_placement,
+        #     log_device_placement=FLAGS.log_device_placement)
+        # sess = tf.Session(config=session_conf)
+        sess = tf.Session()
         with sess.as_default():
-            cnn = TextCNN(
+            cnn = CNN(
                 sequence_length=x_train.shape[1],
                 num_classes=y_train.shape[1],
                 vocab_size=len(vocabulary),
-                embedding_size=FLAGS.embedding_dim,
-                filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
-                num_filters=FLAGS.num_filters,
-                l2_reg_lambda=FLAGS.l2_reg_lambda,
+                embedding_size=EMBEDDING_DIMENSION,
+                filter_sizes=FILTER_SIZES,
+                num_filters=FILTER_COUNT,
                 embedding=embedding_vectors)
 
             # Define Training procedure
@@ -146,97 +117,51 @@ def train(x_train, y_train, vocabulary, x_dev, y_dev, embedding_vectors=None):
             grads_and_vars = optimizer.compute_gradients(cnn.loss)
             train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
-            # Keep track of gradient values and sparsity (optional)
-            grad_summaries = []
-            for g, v in grads_and_vars:
-                if g is not None:
-                    grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
-                    sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name),
-                                                         tf.nn.zero_fraction(g))
-                    grad_summaries.append(grad_hist_summary)
-                    grad_summaries.append(sparsity_summary)
-            grad_summaries_merged = tf.summary.merge(grad_summaries)
-
-            # Output directory for models and summaries
-            timestamp = str(int(time.time()))
-            out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-            print("Writing to {}\n".format(out_dir))
-
-            # Summaries for loss and accuracy
-            loss_summary = tf.summary.scalar("loss", cnn.loss)
-            acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
-
-            # Train Summaries
-            train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
-            train_summary_dir = os.path.join(out_dir, "summaries", "train")
-            train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
-
-            # Dev summaries
-            dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
-            dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
-            dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
-
-            # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
-            checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
-            checkpoint_prefix = os.path.join(checkpoint_dir, "model")
-            if not os.path.exists(checkpoint_dir):
-                os.makedirs(checkpoint_dir)
-            saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
-
-            ## Write vocabulary
-            # vocab_processor.save(os.path.join(out_dir, "vocab"))
-
             # Initialize all variables
             sess.run(tf.global_variables_initializer())
 
-            def train_step(x_batch, y_batch):
+            def train_step(train_data, train_labels):
                 """
                 A single training step
                 """
-                feed_dict = {
-                    cnn.input_x: x_batch,
-                    cnn.input_y: y_batch,
-                    cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
-                }
-                _, step, summaries, loss, accuracy = sess.run(
-                    [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
-                    feed_dict)
-                time_str = datetime.datetime.now().isoformat()
-                print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
-                train_summary_writer.add_summary(summaries, step)
+                step(train_data, train_labels)
 
-            def dev_step(x_batch, y_batch, writer=None):
+            def dev_step(test_data, test_labels):
                 """
-                Evaluates model on a dev set
+                Evaluates model on a test set
                 """
-                feed_dict = {
-                    cnn.input_x: x_batch,
-                    cnn.input_y: y_batch,
-                    cnn.dropout_keep_prob: 1.0
+                step(test_data, test_labels, 1.0, False)
+
+            def step(data, labels, dropout_keep_prob=DROPOUT_KEEP_PROBABILITY,
+                     should_train=True):
+                data_for_step = {
+                    cnn.input_data: data,
+                    cnn.input_labels: labels,
+                    cnn.dropout_keep_prob: dropout_keep_prob
                 }
-                step, summaries, loss, accuracy = sess.run(
-                    [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
-                    feed_dict)
-                time_str = datetime.datetime.now().isoformat()
-                print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
-                if writer:
-                    writer.add_summary(summaries, step)
+                if should_train:
+                    res = sess.run([global_step, cnn.loss, cnn.accuracy, train_op], data_for_step)
+                else:
+                    res = sess.run([global_step, cnn.loss, cnn.accuracy], data_for_step)
+
+                current_time = datetime.datetime.now().isoformat()
+                current_step = res[0]
+                loss = res[1]
+                accuracy = res[2]
+                print(f"{current_time}: step {current_step}, loss {loss}, acc {accuracy}")
 
             # Generate batches
             batches = batch_iter(
-                list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+                list(zip(x_train, y_train)), BATCH_SIZE, EPOCH_COUNT)
             # Training loop. For each batch...
             for batch in batches:
                 x_batch, y_batch = zip(*batch)
                 train_step(x_batch, y_batch)
                 current_step = tf.train.global_step(sess, global_step)
-                if current_step % FLAGS.evaluate_every == 0:
+                if current_step % EVALUATE_EVERY == 0:
                     print("\nEvaluation:")
-                    dev_step(x_dev, y_dev, writer=dev_summary_writer)
+                    dev_step(x_dev, y_dev)
                     print("")
-                if current_step % FLAGS.checkpoint_every == 0:
-                    path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-                    print("Saved model checkpoint to {}\n".format(path))
 
 
 def main(argv=None):
