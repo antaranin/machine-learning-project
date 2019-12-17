f0d6020553c85ee4494c4fc538f1d553f6f25908
antaranin <rafal@getlevelup.com>
2019-12-17 18:23:04 +0100

    Removed some unnecesary object properties. Renamed some variables to be more descriptive.

--- test_cnn.py
+++ test_cnn.py
@@ -32,9 +32,9 @@ class CNN(object):
             embedding_to_use = embedding if embedding is not None \
                 else tf.random_uniform((vocabulary_size, embedding_size), -1.0, 1.0)
 
-            self.W = tf.Variable(embedding_to_use, dtype='float32')
-            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_data)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+            weights = tf.Variable(embedding_to_use, dtype='float32')
+            embedded_vec = tf.nn.embedding_lookup(weights, self.input_data)
+            self.embedded_vectors = tf.expand_dims(embedded_vec, -1)
 
     def _setup_convolutional_pooling_layer(self, filter_sizes: Union[List[int], Tuple[int]],
                                            embedding_size: int,
@@ -47,10 +47,11 @@ class CNN(object):
                                                                         sequence_length)
             pooled_outputs.append(pooled)
 
-        # Combine all the pooled features
         total_filter_count = number_of_filters * len(filter_sizes)
-        self.h_pool = tf.concat(pooled_outputs, 3)
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, total_filter_count])
+        combined_pooled_features = tf.concat(pooled_outputs, 3)
+        self.combined_pooled_features_flattened = tf.reshape(
+            combined_pooled_features, [-1, total_filter_count]
+        )
 
     def _setup_convolutional_pooling_layer_for_filter(self, filter_size: int,
                                                       number_of_filters: int, embedding_size: int,
@@ -60,14 +61,17 @@ class CNN(object):
             weights = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
             bias = tf.Variable(tf.constant(0.1, shape=(number_of_filters,)))
             convolution = tf.nn.conv2d(
-                self.embedded_chars_expanded,
+                self.embedded_vectors,
                 weights,
                 strides=(1, 1, 1, 1),
                 padding="VALID")
 
             feature_map = tf.nn.relu(tf.nn.bias_add(convolution, bias), name="relu")
 
-            #ksize - window size
+            '''
+            ksize - window size, since every filter corresponds to one pooled value, therefore,
+            there has to be size of the sequence - filter size + 1 values in the pooling layer
+            '''
             pooling = tf.nn.max_pool(
                 feature_map,
                 ksize=(1, sequence_length - filter_size + 1, 1, 1),
@@ -77,16 +81,17 @@ class CNN(object):
 
     def _setup_dropout_layer(self):
         with tf.name_scope("dropout"):
-            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+            self.dropout = tf.nn.dropout(self.combined_pooled_features_flattened,
+                                         self.dropout_keep_prob)
 
     def _setup_output_layer(self, total_filter_count: int, number_of_classes: int):
         with tf.name_scope("output"):
             weights = tf.get_variable(
-                "W",
+                "weights",
                 shape=[total_filter_count, number_of_classes],
                 initializer=tf.contrib.layers.xavier_initializer())
             bias = tf.Variable(tf.constant(0.1, shape=[number_of_classes]))
-            self.scores = tf.nn.xw_plus_b(self.h_drop, weights, bias)
+            self.scores = tf.nn.xw_plus_b(self.dropout, weights, bias)
             self.predictions = tf.argmax(self.scores, 1)
 
     def _setup_loss_calculation(self):
