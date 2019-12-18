136a886d0332c5b8b881a02af2cb64839278f582
antaranin <rafal@getlevelup.com>
2019-12-18 13:24:52 +0100

    Removed scope names.

--- test_cnn.py
+++ test_cnn.py
@@ -9,7 +9,8 @@ class CNN(object):
     def __init__(
             self, sequence_length: int, number_of_classes: int, vocabulary_size: int,
             embedding_size: int, filter_sizes: List[int], filter_count: int,
-            embedding: np.ndarray = None):
+            embedding: np.ndarray = None
+    ):
         self._setup_placeholders(sequence_length, number_of_classes)
         self._setup_embedding(vocabulary_size, embedding_size, embedding)
         self._setup_convolutional_pooling_layer(filter_sizes, embedding_size, filter_count,
@@ -28,27 +29,25 @@ class CNN(object):
 
     def _setup_embedding(self, vocabulary_size: int, embedding_size: int,
                          embedding: np.ndarray = None):
-        with tf.name_scope("embedding"):
-            embedding_to_use = embedding if embedding is not None \
-                else tf.random_uniform((vocabulary_size, embedding_size), -1.0, 1.0)
+        embedding_to_use = embedding if embedding is not None \
+            else tf.random_uniform((vocabulary_size, embedding_size), -1.0, 1.0)
 
-            weights = tf.Variable(embedding_to_use, dtype='float32')
-            embedded_vec = tf.nn.embedding_lookup(weights, self.input_data)
-            self.embedded_vectors = tf.expand_dims(embedded_vec, -1)
+        weights = tf.Variable(embedding_to_use, dtype='float32', trainable=False)
+        embedded_vec = tf.nn.embedding_lookup(weights, self.input_data)
+        self.embedded_vectors = tf.expand_dims(embedded_vec, -1)
 
     def _setup_convolutional_pooling_layer(self, filter_sizes: Union[List[int], Tuple[int]],
                                            embedding_size: int,
                                            number_of_filters: int, sequence_length: int):
-        pooled_outputs = []
-        for filter_size in filter_sizes:
-            pooled = self._setup_convolutional_pooling_layer_for_filter(filter_size,
-                                                                        number_of_filters,
-                                                                        embedding_size,
-                                                                        sequence_length)
-            pooled_outputs.append(pooled)
+        pooled_outputs = [self._setup_convolutional_pooling_layer_for_filter(
+            filter_size,
+            number_of_filters,
+            embedding_size,
+            sequence_length
+        ) for filter_size in filter_sizes]
 
         total_filter_count = number_of_filters * len(filter_sizes)
-        combined_pooled_features = tf.concat(pooled_outputs, 3)
+        combined_pooled_features = tf.concat(pooled_outputs, axis=3)
         self.combined_pooled_features_flattened = tf.reshape(
             combined_pooled_features, [-1, total_filter_count]
         )
@@ -56,51 +55,47 @@ class CNN(object):
     def _setup_convolutional_pooling_layer_for_filter(self, filter_size: int,
                                                       number_of_filters: int, embedding_size: int,
                                                       sequence_length: int):
-        with tf.name_scope(f"convolutional-maxpool-for-filter-{filter_size}"):
-            filter_shape = (filter_size, embedding_size, 1, number_of_filters)
-            weights = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
-            bias = tf.Variable(tf.constant(0.1, shape=(number_of_filters,)))
-            convolution = tf.nn.conv2d(
-                self.embedded_vectors,
-                weights,
-                strides=(1, 1, 1, 1),
-                padding="VALID")
+        filter_shape = (filter_size, embedding_size, 1, number_of_filters)
+        weights = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
+        bias = tf.Variable(tf.constant(0.1, shape=(number_of_filters,)))
+        convolution = tf.nn.conv2d(
+            self.embedded_vectors,
+            weights,
+            strides=(1, 1, 1, 1),
+            padding="VALID")
 
-            feature_map = tf.nn.relu(tf.nn.bias_add(convolution, bias), name="relu")
+        convolution_with_bias = tf.nn.bias_add(convolution, bias)
+        feature_map = tf.nn.relu(convolution_with_bias)
 
-            '''
-            ksize - window size, since every filter corresponds to one pooled value, therefore,
-            there has to be size of the sequence - filter size + 1 values in the pooling layer
-            '''
-            pooling = tf.nn.max_pool(
-                feature_map,
-                ksize=(1, sequence_length - filter_size + 1, 1, 1),
-                strides=(1, 1, 1, 1),
-                padding='VALID')
-            return pooling
+        '''
+        ksize - window size, since every filter corresponds to one pooled value, therefore,
+        there has to be size of the sequence - filter size + 1 values in the pooling layer
+        '''
+        pooling = tf.nn.max_pool(
+            feature_map,
+            ksize=(1, sequence_length - filter_size + 1, 1, 1),
+            strides=(1, 1, 1, 1),
+            padding='VALID')
+        return pooling
 
     def _setup_dropout_layer(self):
-        with tf.name_scope("dropout"):
-            self.dropout = tf.nn.dropout(self.combined_pooled_features_flattened,
-                                         self.dropout_keep_prob)
+        self.dropout = tf.nn.dropout(self.combined_pooled_features_flattened,
+                                     self.dropout_keep_prob)
 
     def _setup_output_layer(self, total_filter_count: int, number_of_classes: int):
-        with tf.name_scope("output"):
-            weights = tf.get_variable(
-                "weights",
-                shape=[total_filter_count, number_of_classes],
-                initializer=tf.contrib.layers.xavier_initializer())
-            bias = tf.Variable(tf.constant(0.1, shape=[number_of_classes]))
-            self.scores = tf.nn.xw_plus_b(self.dropout, weights, bias)
-            self.predictions = tf.argmax(self.scores, 1)
+        weights = tf.get_variable(
+            "weights",
+            shape=[total_filter_count, number_of_classes],
+            initializer=tf.contrib.layers.xavier_initializer())
+        bias = tf.Variable(tf.constant(0.1, shape=[number_of_classes]))
+        self.scores = tf.nn.xw_plus_b(self.dropout, weights, bias)
+        self.predictions = tf.argmax(self.scores, 1)
 
     def _setup_loss_calculation(self):
-        with tf.name_scope("loss"):
-            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores,
-                                                             labels=self.input_labels)
-            self.loss = tf.reduce_mean(losses)
+        loss_matrix = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores,
+                                                              labels=self.input_labels)
+        self.loss = tf.reduce_mean(loss_matrix)
 
     def _setup_accuracy_calculation(self):
-        with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_labels, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
+        correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_labels, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
