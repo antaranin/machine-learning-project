99634981f52f5067bff39bf2049a906cad2983f6
antaranin <rafal@getlevelup.com>
2019-12-20 10:32:50 +0100

    Commented out all of the gensim code, since it is not necessary.

--- word2vec_handler.py
+++ word2vec_handler.py
@@ -3,51 +3,57 @@ import os
 from typing import Dict, Iterable, Tuple
 
 import numpy as np
-from gensim.models import KeyedVectors
-from gensim.models.keyedvectors import FastTextKeyedVectors
-
-from config import RANDOM_NORMAL_VECTOR_MEAN, RANDOM_NORMAL_VECTOR_STANDARD_DEV
-from data_importer import load_vocabulary_mr
 
 
-def get_vectors_for_words(model: FastTextKeyedVectors, words: Iterable[str]) -> \
-        Tuple[Dict[str, int], np.ndarray]:
-    words_to_indexes = {}
-    vectors = []
-    index = 0
-    for word in words:
-        words_to_indexes[word] = index
-        if word in model.vocab:
-            vectors.append(model.vectors[model.vocab[word].index])
-        else:
-            vectors.append(
-                np.random.normal(RANDOM_NORMAL_VECTOR_MEAN, RANDOM_NORMAL_VECTOR_STANDARD_DEV, 300)
-            )
-        index += 1
-    return words_to_indexes, np.array(vectors)
+# this code is used for extracting data from word2vec file and as such is not needed for the
+# cnn to run as all of that data has already been saved to mapping.csv and vectors.npy
+
+# from gensim.models import KeyedVectors
+# from gensim.models.keyedvectors import FastTextKeyedVectors
+#
+# from config import RANDOM_NORMAL_VECTOR_MEAN, RANDOM_NORMAL_VECTOR_STANDARD_DEV
+# from data_importer import load_vocabulary_mr
 
 
-def save_vectors_and_index_mapping(word_to_index_mapping: Dict[str, int], vectors: np.ndarray,
-                                   title: str):
-    dir_name = f"vectors/processed/{title}"
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
+# def get_vectors_for_words(model: FastTextKeyedVectors, words: Iterable[str]) -> \
+#         Tuple[Dict[str, int], np.ndarray]:
+#     words_to_indexes = {}
+#     vectors = []
+#     index = 0
+#     for word in words:
+#         words_to_indexes[word] = index
+#         if word in model.vocab:
+#             vectors.append(model.vectors[model.vocab[word].index])
+#         else:
+#             vectors.append(
+#                 np.random.normal(RANDOM_NORMAL_VECTOR_MEAN, RANDOM_NORMAL_VECTOR_STANDARD_DEV, 300)
+#             )
+#         index += 1
+#     return words_to_indexes, np.array(vectors)
+#
+#
+# def save_vectors_and_index_mapping(word_to_index_mapping: Dict[str, int], vectors: np.ndarray,
+#                                    title: str):
+#     dir_name = f"vectors/processed/{title}"
+#     if not os.path.exists(dir_name):
+#         os.makedirs(dir_name)
+#
+#     with open(f'{dir_name}/mapping.csv', mode='w+') as mapping_file:
+#         writer = csv.writer(mapping_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#         for word in word_to_index_mapping:
+#             writer.writerow([word, word_to_index_mapping[word]])
+#     np.save(f"vectors/processed/{title}/vectors.npy", vectors)
+#
+#
+# def save_mr_word_vectors():
+#     mr_vocabulary = load_vocabulary_mr()
+#     words = mr_vocabulary.keys()
+#     model = KeyedVectors.load("vectors/news_vectors.model")
+#     mapping, vectors = get_vectors_for_words(model, words)
+#     save_vectors_and_index_mapping(mapping, vectors, "mr")
 
-    with open(f'{dir_name}/mapping.csv', mode='w+') as mapping_file:
-        writer = csv.writer(mapping_file, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        for word in word_to_index_mapping:
-            writer.writerow([word, word_to_index_mapping[word]])
-    np.save(f"vectors/processed/{title}/vectors.npy", vectors)
 
-
-def save_mr_word_vectors():
-    mr_vocabulary = load_vocabulary_mr()
-    words = mr_vocabulary.keys()
-    model = KeyedVectors.load("vectors/news_vectors.model")
-    mapping, vectors = get_vectors_for_words(model, words)
-    save_vectors_and_index_mapping(mapping, vectors, "mr")
-
-
+# this code is used for extracting data from processed word2vec that is then used for CNN
 def load_mr_word_vectors() -> Tuple[Dict[str, int], np.ndarray]:
     return load_vectors_and_index_mapping("mr")
 
